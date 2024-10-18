84869dbab4a885215e6f5b8ea9734c674d3eb532
vivek <vivekmishra01@gmail.com>
2024-10-18 15:51:08 -0700

    Add Queue, combine test_functions for both

--- src/dsu/ds.py
+++ src/dsu/ds.py
@@ -21,17 +21,46 @@ class Stack:
   def show(self):
     print ("Stack: " , self.stack_)
 
-def test_stack():
+
+class queue:
+  def __init__(self):
+    self.queue_ =[]
+
+  def is_empty(self):
+    return len(self.queue_)==0
+    
+  def peek(self):
+    if not self.is_empty():
+      return self.queue_[0] 
+    return "Empty queue"
+
+  def push(self,value):
+    self.queue_.append(value)
   
+  def pop(self):
+    self.queue_.pop(0)
+
+  def show(self):
+    print ("queue: " , self.queue_)
+
+   
+
+def test_stack():
   s = Stack()
-  s.is_empty()
-  s.push(1)
-  s.push(10)
-  s.push(100)
-  s.show()
-  s.pop()
-  print(s.peek())
-  print(s.is_empty())
+  q = Queue()
+  for a in [s,q]:
+    s.is_empty()
+    s.push(1)
+    s.push(10)
+    s.push(100)
+    s.push(100)
+    s.show()
+    s.pop()
+    print(s.peek())
+    print(s.is_empty())
+
+
+
 
 if __name__=="__main__":
   test_stack();
